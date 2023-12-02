b30a9620fa29cb081f597ad1b5bac7637b9b3686
dogman0121 <99341345+dogman0121@users.noreply.github.com>
2023-12-02 23:23:16 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,24 +1,5 @@
 import sqlite3
-
-
-class Title:
-    def __init__(self, data):
-        self.id = data[0]
-        self.type = data[1]
-        self.status = data[2]
-        self.year = data[3]
-        self.name_russian = data[4]
-        self.name_translation = data[5]
-        self.poster_url = data[6]
-        self.description = data[7]
-        self.rating = data[8]
-        self.views = f'{round(data[9]/1e6, 2)}М' if data[9] >= 1e6 else f'{round(data[9]/1e3, 2)}K' if data[9] >= 1e3 else data[9]
-        self.saved = f'{round(data[10]/1e6, 2)}М' if data[10] >= 1e6 else f'{round(data[10]/1e3, 2)}K' if data[10] >= 1e3 else data[10]
-        self.genres = data[11]
-        self.author = data[12]
-        self.artist = data[13]
-        self.publisher = data[14]
-        self.translator = data[15]
+import json
 
 class Database:
     def __init__(self):
@@ -27,9 +8,54 @@ class Database:
 
     def get_manga_data(self, id):
         data = self.cursor.execute(""" SELECT * FROM titles WHERE id=? """, (id,)).fetchone()
-        if data is None: return None
-        return Title(data)
+        manga_info = dict()
+        manga_info['id'] = str(data[0])
+        manga_info['type'] = data[1]
+        manga_info['status'] = data[2]
+        manga_info['year'] = data[3]
+        manga_info['name_russian'] = data[4]
+        manga_info['name_english'] = data[5]
+        manga_info['name_original'] = data[6]
+        manga_info['description'] = data[7]
+        manga_info['rating'] = data[8]
+        manga_info['views'] = data[9]
+        manga_info['saved'] = data[10]
+        manga_info['genres'] = json.loads(data[11])
+        manga_info['authors'] = json.loads(data[12])
+        manga_info['artist'] = json.loads(data[13])
+        manga_info['publisher'] = json.loads(data[14])
+        manga_info['translator'] = json.loads(data[15])
+        return manga_info
+
+    def add_manga_data(self, data):
+        with open('number.txt', 'r') as file:
+            title_id = int(file.read())
+        with open('number.txt', 'w') as file:
+            file.write(str(title_id+1))
+
+        self.cursor.execute(
+            """ INSERT INTO titles (type, status, year, name_russian, name_english, name_original, description, genres, 
+            author, artist, publisher, translator) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            (title_id, data.type.data, data.status.data, data.status.year, data.name_russian.data,
+             data.name_endlish.data, data.name_original.data, data.description.data, json.loads(data.genres.data),
+             json.loads(data.author.data), json.loads(data.artist.data), json.loads(data.publisher.data),
+             json.loads(data.translator.data,))
+        )
+        self.connection.commit()
+
+    def get_chapters_by_title(self, title_id):
+        return self.cursor.execute(""" SELECT id, title_id, tome, chapter FROM chapters WHERE title_id=? """, (title_id,)).fetchall()
+
+    def get_chapter_by_id(self, chapter_id):
+        return self.cursor.execute(""" SELECT id, title_id, tome, chapter FROM chapters WHERE id=? """, (chapter_id,)).fetchone()
+
+    def get_type_by_id(self, title_id):
+        return self.cursor.execute(""" SELECT type FROM titles WHERE id=? """, (title_id,)).fetchone()
 
     def add_views(self, id):
         self.cursor.execute(""" UPDATE titles SET views=views+1 WHERE id=?""", (id,))
         self.connection.commit()
+
+    def get_genres(self):
+        return self.cursor.execute(""" SELECT * from genres """).fetchall()
+
--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, redirect, url_for
+from PIL import Image
 from database import Database
+from forms import MangaForm
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '?MJritXZS5dm06L6EyfmzQP'
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 db = Database()
 
 @app.route('/manga/<int:title_id>')
 def index(title_id):
     db.add_views(title_id)
-    title = db.get_manga_data(title_id)
-    if title != None:
-        return render_template('manga-card.html', title = title)
-    return '404'
+    manga = db.get_manga_data(title_id)
+    chapters = db.get_chapters_by_title(title_id)
 
+    if manga is None:
+        return '404'
+    return render_template('manga-card.html', title=manga, chapters=chapters)
+
+
+@app.route('/add_manga', methods=['GET', 'POST'])
+def manga_form():
+    form = MangaForm()
+    if form.validate_on_submit():
+        print(form.data)
+        return '1'
+    return render_template('add-manga.html', form=form, genres=db.get_genres())
+
+
+@app.route('/chapters')
+def manga_reader():
+    chapter_id, title_id, tome, chapter = db.get_chapter_by_id(request.args.get('chapter_id'))
+    title_type = db.get_type_by_id(title_id)
+    if title_type[0] == 'Манга':
+        return render_template('chapter_manga.html',
+                               images=os.listdir(f'static\\media\\chapters\\{chapter_id}'),
+                               chapter_id=chapter_id)
+    else:
+        return render_template('chapter_manhwa.html',
+                               images=os.listdir(f'static\\media\\chapters\\{chapter_id}'),
+                               chapter_id=chapter_id)
 
 if '__main__' == __name__:
     app.run()
