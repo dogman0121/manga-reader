46c0283bd945f789ba60ca8b1e03bbd1d51dc6bd
dogman0121 <giachetti101@gmail.com>
2024-10-10 21:50:56 +0300

    Add team model to admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from app import db
-from app.models import User, Title
+from app.models import User, Title, Team
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
@@ -9,3 +9,4 @@ admin = Admin()
 
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Title, db.session))
+admin.add_view(ModelView(Team, db.session))
