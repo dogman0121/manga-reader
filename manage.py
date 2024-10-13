f70a38102ad771505ae7b701e5e32eb6d95ec1d0
dogman0121 <giachetti101@gmail.com>
2024-10-13 12:30:57 +0300

    Fix team members

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
-from config import Config
+from config import TestConfig
 
-app = create_app(Config)
+app = create_app(TestConfig)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
