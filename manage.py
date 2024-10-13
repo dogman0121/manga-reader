7c98647a7c98508ba062c40560cbe539e5d70186
dogman0121 <giachetti101@gmail.com>
2024-10-13 12:37:15 +0300

    Change config

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
-from config import TestConfig
+from config import Config
 
-app = create_app(TestConfig)
+app = create_app(Config)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
