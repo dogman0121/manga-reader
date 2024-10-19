887942c62c22dbac176febfc0ffff447782a6b55
dogman0121 <giachetti101@gmail.com>
2024-10-19 21:04:02 +0300

    Fix add saves bug

--- app/static/manga_card/manga_card/saves.js
+++ app/static/manga_card/manga_card/saves.js
@@ -1,55 +1,79 @@
 let save = document.querySelector(".saved");
-save.addEventListener("click", saveEvent);
+save.addEventListener("click", debounce(saveTitle, 200));
 
-function saveEvent(){
-    let save = document.querySelector(".saved");
-    if (save.querySelector(".stats-option__caption")){
-        let savesMessage = save.querySelector(".stats-option__caption");
-        fetch("../api/save", {
-            method: "DELETE",
-            headers: {
-                'Accept': 'application/json',
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title_id: title_id,
-            }),
-        })
-            .then(response => response.json())
-            .then(status => {
-                let saves = save.querySelector(".stats-option__text");
-                let savesCount = parseInt(saves.textContent);
-                saves.textContent = String(savesCount - 1);
 
-                savesMessage.remove();
-            })
-        return null;
+function debounce(func, timeout){
+    return function call(...args){
+        let previousCall = this.lastCall;
+
+        this.lastCall = Date.now();
+
+        if (previousCall && (this.lastCall - previousCall) < timeout)
+            clearTimeout(this.callTimeout);
+
+        this.callTimeout = setTimeout(() => func(...args), timeout);
     }
-
-    fetch("../api/save", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title_id: title_id,
-            }),
-        })
-            .then(response => response.json())
-            .then(status => {
-                let savedMesageHtml = `
-                    <div class="stats-option__caption">
-                        (сохранено)
-                    </div>
-                `
-                let savedMessageRendered = new DOMParser().parseFromString(savedMesageHtml, "text/html");
-                let savesMessageElement = savedMessageRendered.querySelector(".stats-option__caption");
-
-                let saves = save.querySelector(".stats-option__text");
-                let savesCount = parseInt(saves.textContent);
-                saves.textContent = String(savesCount + 1);
-
-                save.append(savesMessageElement);
-            })
 }
+
+
+function saveTitle() {
+    if (save.querySelector(".stats-option__caption"))
+        removeSave();
+    else
+        addSave();
+}
+
+
+function addSave(){
+    fetch("../api/save", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title_id: title_id,
+        }),
+    })
+        .then(response => response.json())
+        .then(json => {
+            if (json.status !== "ok")
+                return null;
+
+            let savedMessageRendered = new DOMParser().parseFromString(`
+                <div class="stats-option__caption">
+                    (сохранено)
+                </div>
+            `, "text/html").querySelector(".stats-option__caption");
+
+
+            let savesCount = parseInt(save.querySelector(".stats-option__text").textContent);
+            save.querySelector(".stats-option__text").textContent = (savesCount + 1).toString();
+
+            save.append(savedMessageRendered);
+        })
+}
+
+function removeSave(){
+    fetch("../api/save", {
+        method: "DELETE",
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title_id: title_id,
+        }),
+    })
+        .then(response => response.json())
+        .then(json => {
+            if (json.status !== "ok")
+                return null;
+
+            let count = parseInt(save.querySelector(".stats-option__text").textContent);
+            save.querySelector(".stats-option__text").textContent = (count - 1).toString();
+            save.querySelector(".stats-option__caption").remove();
+        })
+}
+
+
