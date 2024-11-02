332dc2731aab5ff8e013a6339a9c6050965fb692
dogman0121 <giachetti101@gmail.com>
2024-11-03 00:06:25 +0300

    Fix image size in chapter

--- app/chapters/forms.py
+++ app/chapters/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
 from wtforms import FileField, StringField, IntegerField, SubmitField
-from wtforms.validators import NumberRange
+from wtforms.validators import NumberRange, InputRequired
 
 
 class ChapterForm(FlaskForm):
-    tome = IntegerField(validators=[NumberRange(min=1)])
-    chapter = IntegerField(validators=[NumberRange(min=1)])
+    tome = IntegerField()
+    chapter = IntegerField()
     name = StringField()
     file = FileField(validators=[FileRequired()])
     save = SubmitField("Сохранить")
