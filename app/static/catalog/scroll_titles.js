0f5708084107338f0b75a8e0ffb0af6ab5a96d8b
dogman0121 <giachetti101@gmail.com>
2024-11-16 00:05:35 +0300

    Fix catalg scroll loading

--- app/static/catalog/scroll_titles.js
+++ app/static/catalog/scroll_titles.js
@@ -1,15 +1,23 @@
+let catalogPage = 1;
+let ended = true;
+
+
 window.addEventListener("scroll", function (){
 
     let titlesGrid = document.querySelector(".catalog__grid");
     let titlesGridCords = titlesGrid.getBoundingClientRect();
     let titlesGridBottom = titlesGridCords.bottom;
     if((titlesGridBottom - document.documentElement.clientHeight) < 10) {
-        getTitles(formData, Math.ceil(catalogTitles.length / 20) + 1)
-            .then((titles) => {
-                let grid = document.querySelector(".catalog__grid");
-                catalogTitles = catalogTitles.concat(titles);
-                for (let title of titles)
-                    grid.innerHTML += createGridBlock(title, gridMode);
-            })
+        if (ended){
+            ended = false;
+            getTitles(formData, ++page)
+                .then((titles) => {
+                    let grid = document.querySelector(".catalog__grid");
+                    catalogTitles = catalogTitles.concat(titles);
+                    for (let title of titles)
+                        grid.innerHTML += createGridBlock(title, gridMode);
+                })
+                .then(() => {ended = true});
+        }
     }
 });
