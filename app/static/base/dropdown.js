8bb3b87e25e83e8b943f26f4f823a20798ab17e2
dogman0121 <giachetti101@gmail.com>
2024-11-21 12:08:56 +0300

    Fix dropdown

--- app/static/base/dropdown.js
+++ app/static/base/dropdown.js
@@ -1,13 +1,13 @@
 let dropDown = document.querySelector(".dropdown-panel");
 dropDown.addEventListener("click", function (event){
-    if (dropDown.querySelector(".dropdown-menu").hidden)
-        dropDown.querySelector(".dropdown-menu").hidden = null;
+    if (!dropDown.querySelector(".dropdown-menu").style.display)
+        dropDown.querySelector(".dropdown-menu").style.display = "none";
     else
-        dropDown.querySelector(".dropdown-menu").hidden = true;
+        dropDown.querySelector(".dropdown-menu").style.display = null;
     event.stopPropagation();
 });
 
 document.body.addEventListener("click", function (){
-    if (!dropDown.querySelector(".dropdown-menu").hidden)
-        dropDown.querySelector(".dropdown-menu").hidden = true;
+    if (!dropDown.querySelector(".dropdown-menu").style.display)
+        dropDown.querySelector(".dropdown-menu").style.display = "none";
 })
