6a4547fe17867d0f1ec8aa724081fec64426ccf6
dogman0121 <giachetti101@gmail.com>
2024-11-24 01:28:09 +0300

    Update docker

---
+++ migrations/versions/d2aeba9bca87_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d2aeba9bca87
+Revises: 92460db087fb
+Create Date: 2024-11-24 01:26:36.535267
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd2aeba9bca87'
+down_revision = '92460db087fb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
