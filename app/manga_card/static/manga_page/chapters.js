5812be7f32fe5db0c4babe30d94f0b7cbbc36a67
dogman0121 <giachetti101@gmail.com>
2024-11-24 21:49:02 +0300

    Fix edit chapters buttons

--- app/manga_card/static/manga_page/chapters.js
+++ app/manga_card/static/manga_page/chapters.js
@@ -197,8 +197,14 @@ class EditChapterSection extends Component {
         return `
             <div class="chapter__links">
                 <a class="chapter-icon__link chapter__link" href="/chapters/${this.chapter.id}/edit">
+                    <svg class="chapter__icon" width="800px" height="800px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.29289 3.70711L1 11V15H5L12.2929 7.70711L8.29289 3.70711Z" fill="currentColor"/>
+                        <path d="M9.70711 2.29289L13.7071 6.29289L15.1716 4.82843C15.702 4.29799 16 3.57857 16 2.82843C16 1.26633 14.7337 0 13.1716 0C12.4214 0 11.702 0.297995 11.1716 0.828428L9.70711 2.29289Z" fill="currentColor"/>
+                    </svg>
+                </a>
+                <a class="chapter-icon__link chapter__link" href="/chapters/${this.chapter.id}/delete">
                     <svg class="chapter__icon" fill="currentColor" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-                     viewBox="0 0 512 512" xml:space="preserve">
+                    viewBox="0 0 512 512" xml:space="preserve">
                         <g>
                             <g>
                             <path d="M307.2,0H204.8c-4.71,0-8.533,5.086-8.533,11.375V25.6c0,4.71,3.823,8.533,8.533,8.533h102.4
@@ -221,12 +227,6 @@ class EditChapterSection extends Component {
                             s-8.533-3.814-8.533-8.533V196.267c0-4.719,3.823-8.533,8.533-8.533s8.533,3.814,8.533,8.533V418.133z"/>
                             </g>
                         </g>
-                    </svg>
-                </a>
-                <a class="chapter-icon__link chapter__link" href="/chapters/${this.chapter.id}/delete">
-                    <svg class="chapter__icon" width="800px" height="800px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.29289 3.70711L1 11V15H5L12.2929 7.70711L8.29289 3.70711Z" fill="currentColor"/>
-                        <path d="M9.70711 2.29289L13.7071 6.29289L15.1716 4.82843C15.702 4.29799 16 3.57857 16 2.82843C16 1.26633 14.7337 0 13.1716 0C12.4214 0 11.702 0.297995 11.1716 0.828428L9.70711 2.29289Z" fill="currentColor"/>
                     </svg>
                 </a>
             </div>
