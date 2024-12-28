1b68fed43f01d72932bdf1bff981b4c9db0b07d0
dogman0121 <giachetti101@gmail.com>
2024-12-28 17:04:51 +0300

    Update errors

--- app/error.py
+++ app/error.py
@@ -7,8 +7,14 @@ def register_error_handlers(app):
 
 
 def error_404(error):
-    return render("errors/404.html")
+    return render("error.html",
+                  error_title="404 Not found",
+                  error_number="404",
+                  error_description="Данная страница не найдена")
 
 
 def error_500(error):
-    return "500"
+    return render("error.html",
+                  error_title="500 Internal error",
+                  error_number="500",
+                  error_description="Произошла ошибка на сервере.")
