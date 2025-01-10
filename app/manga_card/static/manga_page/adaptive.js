3769a9c98640ffafb6a8b51e4e9fa7df447b55be
dogman0121 <giachetti101@gmail.com>
2025-01-11 00:49:34 +0300

    Update adaptive

--- app/manga_card/static/manga_page/adaptive.js
+++ app/manga_card/static/manga_page/adaptive.js
@@ -1,62 +1,27 @@
-window.addEventListener("resize", () => {
-    if (window.innerWidth <= 700){
-        adapt_manga_page();
-    }
-})
+window.addEventListener("resize", adaptManga);
+window.addEventListener("load", adaptManga);
+window.addEventListener("DOMContentLoaded", adaptManga);
 
-if (window.innerWidth <= 700){
-    adapt_manga_page();
-}
-
-function adapt_buttons() {
-    const readButton = document.querySelector("#read-button");
-    const saveButton = document.querySelector(".save-button");
-    const editButton = document.querySelector(".edit-button");
-
-    const buttonsPanel = document.createElement("div");
-    buttonsPanel.className = "buttons-panel";
-
-    buttonsPanel.append(editButton);
-    buttonsPanel.append(readButton);
-    buttonsPanel.append(saveButton);
-
-    document.querySelector("#left-side").append(buttonsPanel);
-}
-
-function adapt_manga_page(){
-    const leftSide = document.querySelector("#left-side");
-    const mangaNames = document.querySelector("#manga-names");
-    const stats = document.querySelector("#stats");
-    const sectionButtons = document.querySelector(".sections__buttons");
-    const sections = document.querySelector(".sections");
-    const sectionContent = document.querySelector(".sections__content");
-
-    if (!sectionButtons.querySelector("#info-button")) {
-        sectionButtons.innerHTML = `
-            <div class="sections-buttons__item" id="info-button">Информация</div>
-        ` + sectionButtons.innerHTML;
-        underLineSection(document.querySelector("#info-button"));
-    }
-
-    if (!document.querySelector("#info")) {
-        let infoSection = document.createElement("div");
-        infoSection.classList.add("section");
-        infoSection.id = "info";
-
-        const description = document.querySelector("#description");
-        const about = document.querySelector("#about");
-        const genresList = document.querySelector("#genres-list");
+function adaptManga() {
+    if (window.innerWidth < 1096) {
         const similar = document.querySelector("#similar");
-        infoSection.append(description);
-        infoSection.append(about);
-        infoSection.append(genresList);
-        infoSection.append(similar);
-        sectionContent.append(infoSection);
-        chooseSection("info");
+        const mangaInfo = document.querySelector("#manga-info");
+        mangaInfo.append(similar);
     }
+    if (window.innerWidth < 872) {
+        // Изменение кнопки чтения
+        if (document.querySelector(".bottom-panel"))
+            return;
+        const bottomPanel = document.createElement("div");
+        bottomPanel.className = "bottom-panel";
 
-    leftSide.append(mangaNames);
-    leftSide.append(stats);
-    leftSide.append(sectionButtons);
-    leftSide.append(sections);
+        const readButton = document.querySelector(".read-button");
+
+        document.body.append(bottomPanel)
+        bottomPanel.append(readButton);
+        // Изменение кнопки рейтинга
+        // const rating = document.querySelector(".rating__button");
+        // const poster = document.querySelector("#poster");
+        // poster.append(rating);
+    }
 }
--- app/static/header/adaptive.js
+++ app/static/header/adaptive.js
@@ -1,24 +1,20 @@
-window.addEventListener("resize", () => {
+window.addEventListener("resize", adaptHeader);
+window.addEventListener("load", adaptHeader);
+window.addEventListener("DOMContentLoaded", adaptHeader);
+
+function adaptHeader(){
     if (window.innerWidth <= 700){
-        adapt_header();
+        const navCatalog = document.querySelector(".nav__catalog");
+        if (navCatalog)
+            navCatalog.remove();
+
+        const navSearch = document.querySelector(".nav__search");
+        navSearch.innerHTML = `
+            <svg class="nav__icon" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`
+
+        const rightNav = document.querySelector(".nav-right__nav");
+        rightNav.prepend(navSearch);
     }
-})
-
-if (window.innerWidth <= 700){
-    adapt_header();
-}
-function adapt_header(){
-    let navCatalog = document.querySelector(".nav__catalog");
-    if (navCatalog)
-        navCatalog.remove();
-    // navCatalog.innerHTML = "<img class='nav__icon' src='/static/header/images/catalog.png'>"
-
-    let navSearch = document.querySelector(".nav__search");
-    navSearch.innerHTML = `
-        <svg class="nav__icon" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>`
-
-    const rightNav = document.querySelector(".nav-right__nav");
-    rightNav.prepend(navSearch);
 }
